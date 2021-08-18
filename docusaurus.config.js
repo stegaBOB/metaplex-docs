2975d35c7d8fe8d6c90b95222157d0693d145a6b
rmshea <8948187+rmshea@users.noreply.github.com>
2021-08-18 14:24:18 -0700

    Initial commit

---
+++ docusaurus.config.js
@@ -0,0 +1,80 @@
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').DocusaurusConfig} */
+module.exports = {
+  title: 'Metaplex Docs',
+  tagline: 'Documentation for the Metaplex frontend and NFT standard.',
+  url: 'https://docs.metaplex.com',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+  organizationName: 'metaplexfoundation',
+  projectName: 'metaplex-docs', 
+  themeConfig: {
+    navbar: {
+      title: 'Metaplex Docs',
+      logo: {
+        alt: 'My Site Logo',
+        src: 'img/meta-white.svg',
+        srcDark: 'img/meta-black.svg',
+      },
+      items: [
+        {
+          href: 'https://github.com/metaplex-foundation/',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [
+        {
+          title: 'Community',
+          items: [
+            {
+              label: 'Discord',
+              href: 'https://discordapp.com/metaplex',
+            },
+            {
+              label: 'Twitter',
+              href: 'https://twitter.com/metaplex',
+            },
+          ],
+        },
+        {
+          title: 'More',
+          items: [
+            {
+              label: 'GitHub',
+              href: 'https://github.com/metaplex-foundation/metaplex',
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} Metaplex Foundation`,
+    },
+    prism: {
+      theme: lightCodeTheme,
+      darkTheme: darkCodeTheme,
+    },
+  },
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      {
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          routeBasePath: '/',
+          editUrl:
+            'https://github.com/metaplex-foundation/metaplex-docs/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      },
+    ],
+  ],
+};
